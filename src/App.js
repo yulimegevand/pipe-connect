d649cb0cdc7141ed1294d445451c0eb4bb73ad41
Martin Pereira Duarte <martinpereiraduarte@gmail.com>
2022-03-15 15:31:32 -0300

    Se agrego menu -Martin

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import PersonasList from './personas/personsList';
-import { BrowserRouter, Link } from 'react-router-dom';
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <header>
-      
-      </header>
-      <body>
-        <PersonasList />
-      </body>
-      
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/personas">Personas</Link>
+            </li>
+            <li>
+              <Link to="/organizaciones">Organizaciones</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/personas" exact element={<PersonasList/>}/>
+          <Route path="/organizaciones" exact element={<PersonasList/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
